1975307bcae2d1f75906f1d295c4b63cd02d7ff8
Jackson Zou <jzou00@stuy.edu>
2019-03-07 10:46:13 -0500

    working bronze problem

--- USACO.java
+++ USACO.java
@@ -32,6 +32,9 @@ public class USACO {
     for (int i = 0; i < move; i++) {
       for (int j = 0; j < 3; j++) {
         moves[i][j] = scanner.nextInt();
+        if (j != 2) {
+          moves[i][j] -= 1;
+        }
         //System.out.print(moves[i][j] + " ");
       }
       //System.out.print("\n");
@@ -39,31 +42,35 @@ public class USACO {
 
     int count = 0;
     while (count < moves.length) {
+      //System.out.println(""+ moves[count][0] + moves[count][1] + moves[count][2]);
       poundDirt(map, moves[count]);
+      count++;
     }
-
+    int f = 0;
     for (int i = 0; i < row; i++) {
       for (int j = 0; j < col; j++) {
-        System.out.print(map[i][j] + " ");
+        if (map[i][j] < elevation) {
+          f+= elevation - map[i][j];
+        }
       }
-      System.out.print("\n");
     }
-    return 0;
+    return f * 72 * 72;
+
   }
 
 
   public static void poundDirt(int[][] map, int[] moves) {
     int largest = 0;
-    for (int i = moves[0]; i < moves[0]+3; i++) {
-      for (int j = moves[1]; j < moves[1]+3; j++) {
+    for (int i = moves[0]; i < moves[0]+3 && i < map.length ; i++) {
+      for (int j = moves[1]; j < moves[1]+3 && j < map[0].length; j++) {
         if (map[i][j] > largest) {
           largest = map[i][j];
         }
       }
     }
     largest -= moves[2];
-    for (int i = moves[0]; i < moves[0]+3; i++) {
-      for (int j = moves[1]; j < moves[1]+3; j++) {
+    for (int i = moves[0]; i < moves[0]+3 && i < map.length; i++) {
+      for (int j = moves[1]; j < moves[1]+3 && j < map[0].length; j++) {
         if (map[i][j] > largest) {
           map[i][j] = largest;
         }
@@ -72,7 +79,7 @@ public class USACO {
   }
   public static void main(String[] args) {
     try {
-      bronze("test.txt");
+      System.out.println(bronze("test.txt"));
     }
     catch (FileNotFoundException e) {
       System.out.println("haha");
